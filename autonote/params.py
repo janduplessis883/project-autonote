cbf81c480d08c908fa895e0898f02e6b4fd2ca41
Jan du Plessis <drjanduplessis@icloud.com>
2024-01-24 19:39:53 +0000

    init

---
+++ autonote/params.py
@@ -0,0 +1,30 @@
+# Path to data files
+import os
+
+project-projectname = 'autonote'
+
+# Folder path for Automation - Git
+LOCAL_GIT_REPO = os.path.join(
+    os.path.expanduser("~"),
+    "code",
+    "janduplessis883",
+    project-projectname,
+)
+
+RAW_DATA = os.path.join(
+    os.path.expanduser("~"),
+    "code",
+    "janduplessis883",
+    project-projectname,
+    "data",
+    "raw"
+)
+
+PROCESSED_DATA = os.path.join(
+    os.path.expanduser("~"),
+    "code",
+    "janduplessis883",
+    project-projectname,
+    "data",
+    "processed"
+)
